05573e04486f026ca1be3160301eba95e05ed3b2
sazid11222 <sazidmunsur8@gmail.com>
2026-07-29 20:47:33 +0600

    Add files via upload
    Another way to Print my dtails

---
+++ 2nd details.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    char Name,Department,University;
+    if("%c%c%c",&Name,&Department,&University)
+    {
+        printf("Sazid\n");
+        printf("CSC\n");
+        printf("UGV\n");
+    }
+    return 0;
+}
+
