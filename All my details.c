8963056a96bb1fc350f61fd5d01baf1de2f203d2
sazid11222 <sazidmunsur8@gmail.com>
2026-07-29 17:55:26 +0600

    Add files via upload
    Didplay all your dtails

---
+++ All my details.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char Name [] = "Sazid";
+    char department [] = "CSC";
+    char Cuniversity [] = "U.G.V";
+    {
+        printf("Sazid\n");
+        printf("CSC\n");
+        printf("U.G.V\n");
+    }
+    return 0;
+}
+
